b1017700d44e12613e8da629df573426ca48232b
jackvijayy <jackvijay1108@gmail.com>
2024-12-30 12:37:13 +0530

    file-manage

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,8 +1,44 @@
+'use client'
+
 import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { usePathname, useRouter } from 'next/navigation'
+import { sortTypes } from '@/constants'
+import { Value } from '@radix-ui/react-select'
+
 
 const Sort = () => {
+  const path=usePathname();
+  const router=useRouter()
+  const handleSort=()=>{
+    router.push(`${path}?sort=${Value}`)
+  }
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+  <SelectTrigger className="sort-select">
+    <SelectValue placeholder={sortTypes[0].value} />
+  </SelectTrigger>
+  <SelectContent className='sort-select-content'>
+    {sortTypes.map((sort)=>(
+       <SelectItem  
+       key={sort.label} 
+       className='shad-select-item'
+       value={sort.value}
+       >
+        {sort.label}
+       </SelectItem>
+
+    ))}
+   
+    
+  </SelectContent>
+</Select>
   )
 }
 
